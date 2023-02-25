fe8becde4185cfaf56773863102d170fe4e85ec3
Andersonpolanco1 <andersonpolancocontreras@gmail.com>
2023-02-25 15:35:08 -0400

    Adding context and models

---
+++ Models/Register.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAPI.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace JwtAuthAPI.Models
+{
+    public class UserRole
+    {
+        public const string ADMIN = "Admin";
+        public const string USER = "User";
+    }
+}
